d30e55912e6cb0a648dca66af4bfbfd769280e5f
benoit delattre <delattreben@gmail.com>
2019-02-17 21:50:10 +0100

    feat(michelin): get list of starred restaurants

--- Source/Scrapping.js
+++ Source/Scrapping.js
@@ -5,7 +5,7 @@ const $ = require('cheerio')
 const fs = require('fs')
 const fetch = require("node-fetch")
 
-async function createJsonURL(){
+async function ListeURLL(){
     request('https://www.relaischateaux.com/fr/site-map/etablissements', (error, response, html) => {
         if(!error && response.statusCode == 200){
             const $ = cheerio.load(html)
@@ -24,16 +24,16 @@ async function createJsonURL(){
 
 //createJsonURL()
 
-function getURL(){
+function URL(){
     const file = fs.readFileSync('url.json')
     const url = JSON.parse(file)
     return url
 }
 
-const url =getURL()
+const url =URL()
 
 
-const getDataFromUrl = async function(url){
+const initialization = async function(url){
     return rp(url)
         .then(function(html){
             let rest0 = null, rest1 = null
@@ -65,36 +65,36 @@ const getDataFromUrl = async function(url){
         })
 }
 
-async function getHotelJSON(){
-    let result = []
+async function scrap(){
+    let hotel2 = []
     for(let i = 0; i < url.links.length; i++)
     {
         console.log('Process : ' + (i+1) + '/' + url.links.length)
-        await getDataFromUrl(url.links[i]).then(value => {
-            result.push(value)
+        await initialization(url.links[i]).then(value => {
+            hotel2.push(value)
         })
     }   
-    let res = []
-    for(let i = 0; i < result.length; i++){
-        if(result[i].name != ""){
-            res.push(result[i])
+    let hotel = []
+    for(let i = 0; i < hotel2.length; i++){
+        if(hotel2[i].name != ""){
+            hotel.push(hotel2[i])
         }
     }
     
-    result = []
-    for (let i = 0; i < res.length; i++) {
-        if(res[i].rest[1].name != null){
-            result.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(res[i].rest[0].name)))
-            result.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(res[i].rest[1].name)))
+    hotel2 = []
+    for (let i = 0; i < hotel.length; i++) {
+        if(hotel[i].rest[1].name != null){
+            hotel2.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(hotel[i].rest[0].name)))
+            hotel2.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(hotel[i].rest[1].name)))
         }
         else{
-            result.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(res[i].rest[0].name)))
-            result.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(res[i].rest[0].name)))
+            hotel2.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(hotel[i].rest[0].name)))
+            hotel2.push(fetch("https://restaurant.michelin.fr/index.php?q=search/autocomplete/" + encodeURI(hotel[i].rest[0].name)))
         }
     }
     let trash = []
-    for (let i = 0; i < result.length; i++) {
-        var response = await result[i]
+    for (let i = 0; i < hotel2.length; i++) {
+        var response = await hotel2[i]
         response = (await response.json())
         if (response.toString().includes("Aucun résultat.")) {
             trash.push(i)
@@ -112,24 +112,24 @@ async function getHotelJSON(){
                 }
                 if (key != null) {
                     response = response[key].split('"')[1]
-                    res[Math.trunc(i/2)].rest[i%2].michelinurl = "https://restaurant.michelin.fr" + response
+                    hotel[Math.trunc(i/2)].rest[i%2].michelinurl = "https://restaurant.michelin.fr" + response
                 }
             }
         }
     }
-    result = []
-    for (let i = 0; i < res.length * 2; i++) {
-        if(res[Math.trunc(i/2)].rest[i%2].michelinurl != null){
-            result.push(fetch(res[Math.trunc(i/2)].rest[i%2].michelinurl))
+    hotel2 = []
+    for (let i = 0; i < hotel.length * 2; i++) {
+        if(hotel[Math.trunc(i/2)].rest[i%2].michelinurl != null){
+            hotel2.push(fetch(hotel[Math.trunc(i/2)].rest[i%2].michelinurl))
         }
         else{
-            result.push(0)
+            hotel2.push(0)
         }
     }
     let star = 0
-    for (let i = 0; i < res.length * 2; i++) {
-        if(result[i] != 0){
-            var response = await result[i]
+    for (let i = 0; i < hotel.length * 2; i++) {
+        if(hotel2[i] != 0){
+            var response = await hotel2[i]
             response = await response.text()
             star = 0
             if (response.includes("icon-cotation1etoile")) {
@@ -141,23 +141,23 @@ async function getHotelJSON(){
             else if (response.includes("icon-cotation3etoiles")) {
                 star = 3
             }
-            res[Math.trunc(i/2)].rest[i%2].star = star
+            hotel[Math.trunc(i/2)].rest[i%2].star = star
         }
         
     }
-    let data = JSON.stringify(res)
+    let data = JSON.stringify(hotel)
     fs.writeFileSync('hotel.json', data)
-    return res
+    return hotel
 }
 
 function getHotel3(){
-    getHotelJSON()
+    scrap()
     const file = fs.readFileSync('hotel.json')
     const url = JSON.parse(file)
     return url
 }
 
-let hotels3 = getHotel3()
+let hotels3 = readJason()
 console.log(hotels3)
 
 
